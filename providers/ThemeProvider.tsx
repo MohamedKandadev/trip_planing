f40ee721593be85cb17eb08797447b1b42c99fa2
MohamedKandadev <kandad.dev@gmail.com>
2024-10-26 09:15:46 +0100

    [ADD] .env to .gitignore

---
+++ providers/ThemeProvider.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const ThemeProvider = (props: Props) => {
+  return (
+    <View>
+      <Text>ThemeProvider</Text>
+    </View>
+  );
+};
+
+export default ThemeProvider;
+
+const styles = StyleSheet.create({});
